1b857ecef02044f33b07c58b5850b17f25e822e5
warchild <wolf@lycanthropy.us>
2024-06-27 15:10:57 -0700

    test

---
+++ Develop/client/src/js/database.js
@@ -0,0 +1,52 @@
+import { openDB } from "idb";
+
+const initdb = async () =>
+  openDB("jate", 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
+        return;
+      }
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await openDB("jate", 1);
+  console.log("PUT to the database");
+
+  const contactDb = await openDB("jate", 1);
+
+  const tx = db.transaction("jate", "readwrite");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.add({ content });
+
+  const result = await request;
+  console.log("result.value", result);
+  return result;
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.log("GET from the database");
+
+  const contactDb = await openDB("jate", 1);
+
+  const tx = contactDb.transaction("jate", "readonly");
+
+  const store = tx.objectStore("jate");
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log("result.value", result);
+  const mostRecentContent = result[result.length - 1].content;
+  console.log("mostRecentContent", mostRecentContent);
+  return mostRecentContent;
+};
+
+initdb();
